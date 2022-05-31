e8bb2c1cac390b22e642decbcd12939964452d8f
dev-rioma <dev.rioma@gmail.com>
2022-05-31 16:44:37 -0500

    Múltiples Peticiones a un API con Callbacks 6/12

--- src/Challenge/challenge.js
+++ src/Challenge/challenge.js
@@ -4,6 +4,8 @@
 //Permite hacer peticiones a algun servidor en la nube
 
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+//Direccion de la API 
+let API = 'https://rickandmortyapi.com/api/character/'
 
 function fetchData(url_api, callback) {
   //referencia al objeto XMLHttpRequest
@@ -53,5 +55,24 @@ function fetchData(url_api, callback) {
   xhttp.send();
 }
 
+// primero buscamos la lista de personajes
+fetchData(API, function (error1, data1) {
+  // si error, matamos retornando un error
+  if (error1) return console.error(error1)
+  // luego buscamos en la api el id de Rick
+  fetchData(API + data1.results[0].id, function (error2, data2) {
+    // si error, matamos retornando un error
+    if (error2) return console.error(error2)
+    // por ultimo la consulta a la api que contiene su dimension
+    fetchData(data2.origin.url, function(error3, data3) {
+      // si error, matamos retornando un error
+      if (error3) return console.error(error3)
 
+      // mostramos los resultados :) 
+      console.log(data1.info.count)
+      console.log(data2.name)
+      console.log(data3.dimension)
+    })
+  })
+})
 
